be9b9f5d3ffe2064497271a605555c2f1f1cfbb4
HenryGJackson <jacksonhg95@gmail.com>
2021-12-21 22:56:40 +0000

    Remove unused code
    Remove unused code

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
+#define ASYNC_SERVER
+
 #include "src/server.h"
 #include "src/serverUtils.h"
 #include <Common/ArgumentParser.h>
 #include <iostream>
 
-bool handleRequest(const std::string& requestedData, std::string& contentOut)
-{
-	contentOut = "This is a test!";
-	return true;
-}
-
-struct MyStruct : public Server::DataRequestHandler
+struct MyTestHandler : public Server::DataRequestHandler
 {
 	virtual bool handle(const std::string& requestedData, std::string& contentOut) const override
 	{
@@ -17,7 +13,7 @@ struct MyStruct : public Server::DataRequestHandler
 		return true;
 	}
 
-	MyStruct() : Server::DataRequestHandler("test") {}
+	MyTestHandler() : Server::DataRequestHandler("test") {}
 };
 
 int main(int argc, char **argv)
@@ -25,13 +21,11 @@ int main(int argc, char **argv)
 	parser::ServerArgParser args;
 	args.parseArguments(argc, argv);
 
-	MyStruct m;
+	MyTestHandler m;
 
 	Server::addDataRequestHandler(&m);
 
 	Server::initiate(args.getPort(), args.getSettingsFile().c_str());
 	Server::join();
 	Server::kill();
-	/*RUN_DEBUG_SERVER(8088);
-	JOIN_DEBUG_SERVER;*/
 }
